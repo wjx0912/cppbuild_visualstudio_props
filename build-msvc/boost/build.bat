72fbed57bc645edb60c8195050ca156d86732af6
wyt <80101277@qq.com>
2024-02-01 16:09:24 +0800

    xx

--- build-msvc/boost/build.bat
+++ build-msvc/boost/build.bat
@@ -12,9 +12,9 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Buil
 b2.exe   toolset=msvc-14.3    --prefix=./build/x86-default/prefix      --stagedir=./build/x86-default/stage       --build-dir=./build/x86-default/build
 b2.exe   toolset=msvc-14.3    --prefix=./build/x86-static/prefix       --stagedir=./build/x86-static/stage        --build-dir=./build/x86-static/build         link=static   runtime-link=static
 
-rm  -fr  .\build\x86-default\build\
-rm  -fr  .\build\x86-static\build\
-rm  -fr  .\build\x64-default\build\
-rm  -fr  .\build\x64-static\build\
+rm  -fr  ./build/x86-default/build/
+rm  -fr  ./build/x86-static/build/
+rm  -fr  ./build/x64-default/build/
+rm  -fr  ./build/x64-static/build/
 
 pause
